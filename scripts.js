696f839d44164d786cf144402898eb0673a9ab0c
Toni Alves <antonio31alves@gmail.com>
2025-04-09 14:13:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && (location.protocol === 'https:' || location.hostname === 'localhost')) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/schi-offline-portfolio/sw.js')
       .then(registration => {
         console.log('Service Worker registered:', registration);
       })
@@ -8,4 +8,6 @@ if ('serviceWorker' in navigator) {
         console.error('Service Worker registration failed:', error);
       });
   });
+} else {
+  console.log('Service Worker not supported or not running in a secure context. Offline mode unavailable.');
 }
