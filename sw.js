57ec7c1ff730380222d54472e175ba73d7bdd226
Toni Alves <antonio31alves@gmail.com>
2025-04-09 14:12:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 const CACHE_NAME = 'minimal-portfolio-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
-  '/images/logo.png',
-  '/images/icon-192.png',
-  '/images/icon-512.png',
-  '/images/window1.jpg',
-  '/images/door1.jpg',
-  '/images/project1.jpg',
-  '/images/brochure1-cover.jpg',
-  '/images/brochure2-cover.jpg',
-  '/images/brochure3-cover.jpg',
-  '/brochures/brochure1.pdf',
-  '/brochures/brochure2.pdf',
-  '/brochures/brochure3.pdf'
+  '/schi-offline-portfolio/',
+  '/schi-offline-portfolio/index.html',
+  '/schi-offline-portfolio/styles.css',
+  '/schi-offline-portfolio/script.js',
+  '/schi-offline-portfolio/manifest.json',
+  '/schi-offline-portfolio/images/logo.png',
+  '/schi-offline-portfolio/images/icon-192.png',
+  '/schi-offline-portfolio/images/icon-512.png',
+  '/schi-offline-portfolio/images/window1.jpg',
+  '/schi-offline-portfolio/images/door1.jpg',
+  '/schi-offline-portfolio/images/project1.jpg',
+  '/schi-offline-portfolio/images/brochure1-cover.jpg',
+  '/schi-offline-portfolio/images/brochure2-cover.jpg',
+  '/schi-offline-portfolio/images/brochure3-cover.jpg',
+  '/schi-offline-portfolio/brochures/brochure1.pdf',
+  '/schi-offline-portfolio/brochures/brochure2.pdf',
+  '/schi-offline-portfolio/brochures/brochure3.pdf'
 ];
 
 self.addEventListener('install', event => {
